d531cb7ce90c5d4d1d84464124a8579e38d2ee68
JonaRBL <121039246@upq.edu.mx>
2023-06-28 16:45:14 -0600

    Pract7: creamos la consulta de los albums y la tabla que los mostrara en la vista

--- pract/app.py
+++ pract/app.py
@@ -16,7 +16,11 @@ mysql = MySQL(app)
 #declaracion de ruta http://localhost:5000
 @app.route('/')
 def index():
-    return render_template('index.html')
+    CC = mysql.connection.cursor()
+    CC.execute('select * from tbalbums')
+    conAlbums = CC.fetchall()
+    print(conAlbums)
+    return render_template('index.html',listAlbums = conAlbums)
 
 #ruta http:localhost:5000/guardar tipo POST para Insert
 @app.route('/guardar', methods=['POST'])
